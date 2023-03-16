66b7ea97d806d96a84da703211c721ba7be9decf
Malte Linke <me@parzival.space>
2023-03-16 18:12:53 +0100

    added some error handling

--- shared/discord/GameDetector.cs
+++ shared/discord/GameDetector.cs
@@ -30,76 +30,93 @@ namespace RPCProxy.Shared.Discord
     /// Re-implementation of Discord game detection for non-RPC games.
     /// </summary>
     /// <param name="log"></param>
-    public GameDetector(ILogger? log = null) {
+    public GameDetector(ILogger? log = null)
+    {
       this.log = log;
 
-      if (!OperatingSystem.IsWindows()) {
+      if (!OperatingSystem.IsWindows())
+      {
         this.log?.LogDebug($"This functionality is only available on Windows.");
         return;
       }
 
-      this.registerWatchers();
+      this.RegisterWatchers();
     }
 
-    private List<Game> downloadDetectableList() {
-      using (HttpClient client = new HttpClient())
-      using (HttpRequestMessage request = new HttpRequestMessage()) {
-        // configure request
-        request.RequestUri = new Uri(API_PATH);
-        request.Method = HttpMethod.Get;
+    private List<Game> DownloadDetectableList()
+    {
+      List<Game> detectList = new List<Game>();
 
-        // add headers
-        request.Headers.Add("Accept", "application/json");
-        request.Headers.Add("User-Agent", USER_AGENT);
+      try
+      {
+        using (HttpClient client = new HttpClient())
+        using (HttpRequestMessage request = new HttpRequestMessage())
+        {
+          // configure request
+          request.RequestUri = new Uri(API_PATH);
+          request.Method = HttpMethod.Get;
 
-        // make request
-        var response = client.SendAsync(request).Result;
-        if (response.IsSuccessStatusCode) {
-          string jsonData = response.Content.ReadAsStringAsync().Result;
-          return JsonConvert.DeserializeObject<List<Game>>(jsonData) ?? new List<Game>();
+          // add headers
+          request.Headers.Add("Accept", "application/json");
+          request.Headers.Add("User-Agent", USER_AGENT);
+
+          // make request
+          HttpResponseMessage response = client.SendAsync(request).Result;
+          if (response.IsSuccessStatusCode)
+          {
+            string jsonData = response.Content.ReadAsStringAsync().Result;
+            detectList = JsonConvert.DeserializeObject<List<Game>>(jsonData) ?? new List<Game>();
+          }
         }
-
-        return new List<Game>();
       }
+      catch (HttpRequestException e) {
+        // ignore error, but print to console
+        this.log?.LogError($"Failed to download list of detectable games.");
+      }
+
+      return detectableList!;
     }
 
-    private void registerWatchers() {
+    private void RegisterWatchers()
+    {
       if (!OperatingSystem.IsWindows()) return;
 
       // watch started processes
       startWatcher = new ManagementEventWatcher(@"\\.\root\CIMV2", "SELECT * FROM __InstanceCreationEvent WITHIN .025 WHERE TargetInstance ISA 'Win32_Process'");
       stopWatcher = new ManagementEventWatcher(@"\\.\root\CIMV2", "SELECT * FROM __InstanceDeletionEvent WITHIN .025 WHERE TargetInstance ISA 'Win32_Process'");
 
-
       /// those are theoretically better because they wont "skip" process events, but require elevated privileges
       // startWatcher = new ManagementEventWatcher(@"\\.\root\CIMV2", "SELECT * FROM Win32_ProcessStartTrace");
       // stopWatcher = new ManagementEventWatcher(@"\\.\root\CIMV2", "SELECT * FROM Win32_ProcessStopTrace");
 
       // begin scan
-      #pragma warning disable CA1416 // Compiler is annoying
+#pragma warning disable CA1416 // Compiler is annoying
       startWatcher.EventArrived += (s, e) =>
       {
-        Game? detectedGame = this.detectableList!.FirstOrDefault(g => g!.Executables.Any(ex => this.gameFilter(e.NewEvent, ex)), null);
-        
-        if (detectedGame != null) {
+        Game? detectedGame = this.detectableList!.FirstOrDefault(g => g!.Executables.Any(ex => this.CheckGameFilter(e.NewEvent, ex)), null);
+
+        if (detectedGame != null)
+        {
           this.log?.LogDebug($"Game {detectedGame.Name} with id {detectedGame.ID} has been started.");
           this.OnGameStarted?.Invoke(detectedGame);
         }
       };
       stopWatcher.EventArrived += (s, e) =>
       {
-        Game? detectedGame = this.detectableList!.FirstOrDefault(g => g!.Executables.Any(ex => this.gameFilter(e.NewEvent, ex)), null);
+        Game? detectedGame = this.detectableList!.FirstOrDefault(g => g!.Executables.Any(ex => this.CheckGameFilter(e.NewEvent, ex)), null);
 
-        if (detectedGame != null) {
+        if (detectedGame != null)
+        {
           this.log?.LogDebug($"Game {detectedGame.Name} with id {detectedGame.ID} has been closed.");
           this.OnGameStopped?.Invoke(detectedGame);
         }
       };
-      #pragma warning restore CA1416
+#pragma warning restore CA1416
     }
 
-    private bool gameFilter(ManagementBaseObject baseObject, Executable executable) {
-      #pragma warning disable CA1416 // Compiler is annoying
+    private bool CheckGameFilter(ManagementBaseObject baseObject, Executable executable)
+    {
+#pragma warning disable CA1416 // Compiler is annoying
 
       // skip darwin checks
       if (executable.OS != "win32") return false;
@@ -109,10 +126,13 @@ namespace RPCProxy.Shared.Discord
       // check args
       bool argsValid = false;
       string argsString = (targetInstance["CommandLine"].ToString() ?? "");
-      if (executable.Arguments != null) {
+      if (executable.Arguments != null)
+      {
         // check args
         argsValid = argsString.Contains(executable.Arguments);
-      } else {
+      }
+      else
+      {
         // ignore args
         argsValid = true;
       }
@@ -120,27 +140,32 @@ namespace RPCProxy.Shared.Discord
       // check path
       bool pathValid = false;
       string pathString = (targetInstance["ExecutablePath"].ToString() ?? "").ToLower().Replace("\\", "/");
-      if (executable.Name.StartsWith(">")) {
+      if (executable.Name.StartsWith(">"))
+      {
         // check if it ends with x
         pathValid = pathString.EndsWith(executable.Name.Substring(1));
-      } else {
+      }
+      else
+      {
         // check if contains x
         pathValid = pathString.Contains(executable.Name);
       }
 
       return pathValid && argsValid;
-      #pragma warning restore CA1416
+#pragma warning restore CA1416
     }
 
     /// <summary>
     /// Starts the detection service.
     /// </summary>
-    public void Start() {
+    public void Start()
+    {
       if (!OperatingSystem.IsWindows()) return;
 
-      if (this.detectableList == null) {
+      if (this.detectableList == null)
+      {
         this.log?.LogDebug("Downloading list of detectable games...");
-        this.detectableList = this.downloadDetectableList();
+        this.detectableList = this.DownloadDetectableList();
       }
 
       if (this.startWatcher != null) this.startWatcher.Start();
@@ -150,9 +175,10 @@ namespace RPCProxy.Shared.Discord
     /// <summary>
     /// Stops the detection service.
     /// </summary>
-    public void Stop() {
+    public void Stop()
+    {
       if (!OperatingSystem.IsWindows()) return;
-      
+
       if (this.startWatcher != null) this.startWatcher.Stop();
       if (this.stopWatcher != null) this.stopWatcher.Stop();
     }
